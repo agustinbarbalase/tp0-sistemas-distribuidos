7d85cb1e307e318bf5d9efa3fa6911e21df110e0
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-08-22 10:13:04 -0300

    feat(ej1): added into mi-generador define_client()

--- mi-generador.py
+++ mi-generador.py
@@ -14,5 +14,21 @@ def define_server(file):
     "\n"
   )
 
+def define_client(file, number_of_client):
+  file.write(
+    f"  client{number_of_client}:\n"
+    f"   container_name: client{number_of_client}\n"
+    "    image: client:latest\n"
+    "    entrypoint: /client\n"
+    "    environment:\n"
+    f"     - CLI_ID={number_of_client}\n"
+    "      - CLI_LOG_LEVEL=DEBUG\n"
+    "    networks:\n"
+    "      - testing_net\n"
+    "    depends_on:\n"
+    "      - server\n"
+    "\n"
+  )
+
 if __name__ == "__main__":
   pass
