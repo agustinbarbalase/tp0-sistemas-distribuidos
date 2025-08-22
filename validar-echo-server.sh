b0c75e3cf06ca785b89b5754bf3cb919266b36d4
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-08-22 15:19:10 -0300

    feat(ej3): changed harcoded vars

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-docker run --network=tp0_testing_net --rm 0ed463b26dae sh -c "echo 'Hola' | nc server 12345"
+
+NETWORK_NAME="tp0_testing_net"
+DOCKER_IMAGE="busybox"
+MESSAGE_FOR_SERVER="You talking to me?" # Travis Bickle (Robert De Niro) - Taxi Driver (1976)
+SERVER_ADDRESS="server"
+SERVER_PORT="12345"
+
+docker run --network=$NETWORK_NAME --rm $DOCKER_IMAGE sh -c "echo '$MESSAGE_FOR_SERVER' | nc $SERVER_ADDRESS $SERVER_PORT"
