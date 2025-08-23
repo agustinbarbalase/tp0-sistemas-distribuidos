36c9e1dce1f49ce519cde9b2c15f6afeb248c133
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-08-22 21:07:15 -0300

    feat(ej4): added signum and frame for signal

--- server/common/server.py
+++ server/common/server.py
@@ -58,7 +58,7 @@ class Server:
         logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
         return c
 
-    def __shutdown(self):
+    def __shutdown(self, signum, frame):
         """
         Gracefull shutdown
 
