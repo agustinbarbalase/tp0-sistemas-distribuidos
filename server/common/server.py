19ad696e3d99a47f968594d84bcd69955292a7bd
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-08-27 11:41:00 -0300

    feat(ej4): deleted unnecesary SIGINT signal

--- server/common/server.py
+++ server/common/server.py
@@ -14,7 +14,6 @@ class Server:
             self.__shutdown()
 
         signal.signal(signal.SIGTERM, handle_signal)
-        signal.signal(signal.SIGINT, handle_signal)
 
     def run(self):
         """
