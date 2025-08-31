03631f55e40fc48c435c5fd89688f619e9b14e57
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-08-30 21:44:50 -0300

    feat(ej8): implemented basic concurrency

--- server/common/server.py
+++ server/common/server.py
@@ -1,18 +1,27 @@
 import socket
 import logging
 import signal
+import multiprocessing
 
 from .protocol import Protocol, UnexpectedMessage, ConnectionClose
 from .utils import store_bets, load_bets, has_won
+
 
 class Server:
     def __init__(self, port, listen_backlog, amount_of_clients):
         # Initialize server socket
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._server_socket.bind(('', port))
+        self._server_socket.bind(("", port))
         self._server_socket.listen(listen_backlog)
         self._amount_of_clients = amount_of_clients
         self._client_sockets = {}
+        self._client = []
+        self._barrier_for_winners = multiprocessing.Barrier(amount_of_clients + 1)
+        self._barrier_for_finish_connection = multiprocessing.Barrier(
+            amount_of_clients + 1
+        )
+        self._store_lock = multiprocessing.Lock()
+        self._load_lock = multiprocessing.Lock()
         self._is_closed = False
 
         def handle_signal(signum, frame):
@@ -31,9 +40,24 @@ class Server:
 
         while not self._is_closed:
             client_sock = self.__accept_new_connection()
-            if self._is_closed: break
-            self.__handle_client_connection(client_sock)
-            if len(self._client_sockets) == self._amount_of_clients:
+            if self._is_closed:
+                break
+            thread = multiprocessing.Process(
+                target=self.__handle_client_connection,
+                args=(
+                    client_sock,
+                    self._barrier_for_winners,
+                    self._barrier_for_finish_connection,
+                    self._store_lock,
+                    self._load_lock,
+                ),
+            )
+            thread.start()
+            self._client.append(thread)
+            if len(self._client) == self._amount_of_clients:
+                self._barrier_for_winners.wait()
+                for t in self._client:
+                    t.join()
                 self.__announce_winners()
 
     def __shutdown(self):
@@ -42,11 +66,11 @@ class Server:
 
         Function used for graceful shutdown of the server
         """
-        logging.info('action: shutdown | result: in_progress')
+        logging.info("action: shutdown | result: in_progress")
         self._is_closed = True
         self._server_socket.shutdown(socket.SHUT_RDWR)
         self._server_socket.close()
-        logging.info('action: shutdown | result: success')
+        logging.info("action: shutdown | result: success")
 
     def __announce_winners(self):
         """
@@ -62,8 +86,10 @@ class Server:
                     protocol = Protocol(client_sock)
                     protocol.send_winner(bet)
                 except Exception as e:
-                    logging.error(f"action: receive_message | result: fail | error: {e}")
-        
+                    logging.error(
+                        f"action: receive_message | result: fail | error: {e}"
+                    )
+
         for client_socket in self._client_sockets.values():
             try:
                 protocol = Protocol(client_socket)
@@ -73,7 +99,12 @@ class Server:
             finally:
                 client_socket.close()
 
-    def __handle_client_connection(self, client_sock):
+    def __handle_client_connection(
+        self,
+        client_sock,
+        barrier_for_winners,
+        store_lock,
+    ):
         """
         Read message from a specific client socket and closes the socket
 
@@ -87,17 +118,24 @@ class Server:
 
             while True:
                 bets, errors = protocol.recv_batch_bets()
-                store_bets(bets)
-                
+                with store_lock:
+                    store_bets(bets)
+
                 if errors > 0:
-                    logging.error(f"action: apuesta_recibida | result: fail | cantidad: {len(bets)}")
+                    logging.error(
+                        f"action: apuesta_recibida | result: fail | cantidad: {len(bets)}"
+                    )
                     protocol.send_failure_msg(len(bets))
                 elif len(bets) > 0:
-                    logging.info(f"action: apuesta_recibida | result: success | cantidad: {len(bets)}")
+                    logging.info(
+                        f"action: apuesta_recibida | result: success | cantidad: {len(bets)}"
+                    )
                     protocol.send_success_msg(len(bets))
                 elif len(bets) == 0:
                     logging.info("action: esperando_ganador | result: success")
                     break
+
+            barrier_for_winners.wait()
 
         except UnexpectedMessage as e:
             logging.error(f"action: receive_message | result: fail | error: {e}")
@@ -118,12 +156,16 @@ class Server:
 
         try:
             # Connection arrived
-            logging.info('action: accept_connections | result: in_progress')
+            logging.info("action: accept_connections | result: in_progress")
             c, addr = self._server_socket.accept()
-            logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
+            logging.info(
+                f"action: accept_connections | result: success | ip: {addr[0]}"
+            )
             return c
         except OSError as err:
-            if not self._is_closed: 
-                logging.error(f"action: accept_connections | result: fail | error: {err}")
+            if not self._is_closed:
+                logging.error(
+                    f"action: accept_connections | result: fail | error: {err}"
+                )
                 raise err
             return None
