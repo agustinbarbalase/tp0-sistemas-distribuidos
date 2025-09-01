5468ececcf54ef581fae7f7fcacc4edd1ff2e15c
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-09-01 15:55:55 -0300

    feat(ej6): using batchIterator and changed functionality

--- client/common/batch.go
+++ client/common/batch.go
@@ -3,16 +3,14 @@ package common
 type Batch struct {
 	Bets      []*Bet
 	MaxAmount int
-	MaxSize   int
 	Amount    int
 	Size      int
 }
 
-func NewBatch(MaxAmount int, MaxSize int) *Batch {
+func NewBatch(MaxAmount int) *Batch {
 	return &Batch{
 		Bets:      make([]*Bet, 0),
 		MaxAmount: MaxAmount,
-		MaxSize:   MaxSize,
 		Amount:    0,
 		Size:      SIZE_HEADER_BYTES + SIZE_LENGTH_BYTES,
 	}
@@ -20,7 +18,7 @@ func NewBatch(MaxAmount int, MaxSize int) *Batch {
 
 func (b *Batch) AddBet(ID string, bet *Bet) bool {
 	lengthPackagedBet := bet.PackagedBetLength(ID)
-	if b.Amount+1 <= b.MaxAmount && b.Size+lengthPackagedBet <= b.MaxSize {
+	if b.Amount+1 <= b.MaxAmount && b.Size+lengthPackagedBet <= MAX_SIZE_PACKAGE_IN_BYTES {
 		b.Size += lengthPackagedBet
 		b.Amount++
 		b.Bets = append(b.Bets, bet)
